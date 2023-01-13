92bf3bbe73c704449ebe00d4e9941d7859d84117
Donia3012v <112786733+Donia3012v@users.noreply.github.com>
2023-01-13 14:33:23 +0200

    Add files via upload

---
+++ LAb1.js
@@ -0,0 +1,8 @@
+var firstName = window.prompt("Enter your FirstName");
+var LastName = window.prompt("Enter your LastName");
+var fullName = firstName +" " + LastName;
+alert( "Your Full Name is: " + " " + fullName)
+var birth = window.prompt("Please Enter your Birth year")
+var age = 2023 - birth;
+alert("Welcome"+ " " + fullName+ " " + "you are" + " " + age + " " +  "years old ")
+document.write("Welcome"+ " " + fullName+ " " + "you are" + " " + age + " " +  "years old ");
