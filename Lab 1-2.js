ab8960e8c98b8a80c7e36389a5dde4f905fe0a37
Donia3012v <112786733+Donia3012v@users.noreply.github.com>
2023-01-13 14:32:27 +0200

    Add files via upload

---
+++ Lab 1-2.js
@@ -0,0 +1,5 @@
+alert("that it’s the first release of a calculator that only has a summation feature");
+var firstNumber = Number(window.prompt("Enter The First Number: "));
+var lastNumber = Number(window.prompt("Enter The Last Number: "));
+var result = firstNumber + lastNumber;
+alert(`The result is: ${result}`);
